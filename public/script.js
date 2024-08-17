7f551f66a93aed756051871eebf102e21c11e046
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-08-17 19:54:35 +0330

    Update script.js

--- public/script.js
+++ public/script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     generateBtn.addEventListener('click', function() {
         const selectedGameIndex = gameSelector.value;
-        fetch('/generate-key', {
+        fetch('https://zircon-periodic-background.glitch.me/generate-key', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
